1d92fc51e656ea4db51f68a85edc08de9081eed7
Robin <robin.majoor@protonmail.com>
2023-08-20 12:03:59 +0200

    Fixed serverless function for ETH price fetching.

--- src/contexts/ETHPriceContext.js
+++ src/contexts/ETHPriceContext.js
@@ -13,23 +13,20 @@ export const ETHPriceProvider = ({ children }) => {
       try {
         // First, try to fetch the ETH price from our serverless function
         const response = await fetch('https://memetool.vercel.app/api/ethPrice');
-        console.log("Response:", response);
         const data = await response.json();
-        console.log("Data:", data);
 
-        if (data && data.ethPrice) {
-          console.log("Setting ETH Price:", data.ethPrice);
-          setETHPrice(data.ethPrice);
+        if (data && data.ethereum && data.ethereum.usd) {
+          console.log("Setting ETH Price:", data.ethereum.usd);
+          setETHPrice(data.ethereum.usd);
           return;
         }
 
         // If the above fails, throw an error to move to the catch block
         throw new Error('Failed to fetch from serverless function');
       } catch (error) {
-        console.error("Error fetching ETH price:", error.message);
         // If fetching from the serverless function fails, use the fetchETHPrice utility as a backup
-        //const backupPrice = await fetchETHPrice();
-        //setETHPrice(backupPrice);
+        const backupPrice = await fetchETHPrice();
+        setETHPrice(backupPrice);
       }
     };
 
