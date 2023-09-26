a7a6e116e78ce78450ab83a19730b8617b77f0b4
Robin <robin.majoor@protonmail.com>
2023-09-26 16:05:31 +0200

    deleted old Landingpage

--- app/page.js
+++ app/page.js
@@ -1,71 +1,59 @@
 'use client'
-// app/page.js
+// app/app/page.js
 
-import styles from '../src/styles/Page.module.css';
-import Link from 'next/link'
+import React from 'react';
+import Dashboard from '../src/components/Dashboard';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../src/styles/theme';
 
-import { Button, Grid, Card, Typography, Box, } from '@mui/material';
-import TwitterIcon from '@mui/icons-material/Twitter';
+// Get current Address from connected Wallet
+import { WalletProvider } from '../src/contexts/WalletContext';
+// Get current ETH price from Coingecko
+import { ETHPriceProvider } from '../src/contexts/ETHPriceContext';
+// Get currently owned Memeland NFTs from connected Wallet
+import { OwnedNFTsProvider } from '../src/contexts/OwnedNFTsContext';
 
+// WalletConnect and Web3Modal imports
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+import { Web3Modal } from '@web3modal/react';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
 
-import ParticlesComponent from '../src/components/ParticlesComponent';
+// Alchemy SDK imports
+import { AlchemyProvider } from '../src/contexts/AlchemyContext';
 
-const logo = "/Black_and_White_Skull_memetool_Animated_Logo_trans.svg";
-const potatoz = "/potatoz_building.webp";
+import '../src/styles/_app.css';
+import '../src/styles/App.css';
 
-function Page() {
+// Configuration for WalletConnect
+const chains = [mainnet];
+const projectId = '398501a83dfb5e2d04ae8a9a355a8587';
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
+export function App({ Component, pageProps }) {
   return (
-    <Box>
-    <Box className={styles.Page}>
-      <ParticlesComponent className={styles.particles} />
-      <Card 
-  sx={{
-    zIndex: 1, 
-    minHeight: 145, 
-    minWidth: 140, 
-    bgcolor: 'rgba(31, 30, 31, 0.8)', 
-    p: 2, 
-    border: 1, 
-    borderColor: '#282928',
-    display: 'flex',         
-    flexDirection: 'column', 
-    alignItems: 'center',    
-    justifyContent: 'center',
-    position: 'relative'     // Set position to relative
-  }}
->
-  <Box sx={{ mt: -2 }}>
-    <img src={logo} alt="Your Logo" />
-  </Box>
-  <Typography variant="body1" letterSpacing={0} gutterBottom sx={{ color: '#E2E3E3', pr: '10px', textAlign: 'center' }}>
-  Crafting Epicness for the <br/>MEMELAND Legends!
-  </Typography>
-  <Box sx={{ 
-    position: 'absolute',   // Set position to absolute
-    bottom: '-12px',            // Adjust as needed
-    right: '-5px',          // Adjust as needed
-    // You can also use bottom and left properties if needed
-  }}>
-    <img src={potatoz} alt="Building Potatoz" width="80" height="80" />
-  </Box>
-  <Box sx={{ mt: 2 ,color: '#E2E3E3' }}>
-    <Link className="noUnderline" href="https://twitter.com/memetoolai" passHref style={{ textDecoration: 'none' }}>
-        <Button 
-          variant="contained" 
-          color="primary" 
-          startIcon={<TwitterIcon sx={{ color: '#E2E3E3' }} />}
-          sx={{ color: '#E2E3E3', textDecoration: 'none' }} style={{ textDecoration: 'none' }}
-        >
-          Follow Us
-        </Button>
-    </Link>
-  </Box>
-</Card>
-
-    </Box>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <WagmiConfig config={wagmiConfig}>
+        <WalletProvider>
+          <ETHPriceProvider>
+            <AlchemyProvider>
+              <OwnedNFTsProvider>
+                <Dashboard>
+                  <Component {...pageProps} />
+                </Dashboard>
+                <Web3Modal projectId={projectId} ethereumClient={ethereumClient} themeMode="dark" />
+              </OwnedNFTsProvider>
+            </AlchemyProvider>
+          </ETHPriceProvider>
+        </WalletProvider>
+      </WagmiConfig>
+    </ThemeProvider>
   );
 }
-
-export default Page;
+export default App;
