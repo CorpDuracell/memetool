d1fda1f1995d3449df0c5cdcf57ee7865e3f66aa
Robin <robin.majoor@protonmail.com>
2023-10-09 16:26:33 +0200

    fixed a bug when sending a question per click in the chat.

--- src/components/ChatLayout.js
+++ src/components/ChatLayout.js
@@ -75,6 +75,9 @@ const ChatLayout = () => {
   };
 
   const handleSend = (content = message) => {
+    if (typeof content === 'object') {
+      content = message;
+    }
     streamMessages(content);
     setMessage('');
   };
