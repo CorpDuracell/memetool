d239f06f2275ed2febccead3591de3f26cb78bbe
Robin <robin.majoor@protonmail.com>
2023-10-11 14:54:27 +0200

    added back height to content css

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,9 @@
 // /src/components/Dashboard.js 
-
 import React, { useState } from 'react';
-//import ChartComponent from './ChartComponent';
-
-// Import components 
 import { DrawerProvider } from '../contexts/DrawerContext';
 import ResponsiveAppBar from './Header';
 import Sidebar from './Sidebar';
 import Content from './Content';
-
-
 
 export default function Dashboard() {
 
@@ -17,16 +11,9 @@ const [activeMenu, setActiveMenu] = useState("Dashboard");
 
   return (
     <DrawerProvider>
-
-    <div className="app">
-
       <ResponsiveAppBar />
-
       <Sidebar activeMenu={activeMenu} setActiveMenu={setActiveMenu} />   
-
       <Content activeMenu={activeMenu} setActiveMenu={setActiveMenu} />  
-
-    </div>
     </DrawerProvider>
   );
 }
